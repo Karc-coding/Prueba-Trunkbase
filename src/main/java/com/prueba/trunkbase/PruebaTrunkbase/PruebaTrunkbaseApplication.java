26849eea035cd28d2f8b71e19616e0c164241d76
Karc <kevinrojascobenas@gmail.com>
2022-09-19 12:15:24 -0500

    First commit

---
+++ src/main/java/com/prueba/trunkbase/PruebaTrunkbase/PruebaTrunkbaseApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.trunkbase.PruebaTrunkbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTrunkbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTrunkbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/trunkbase/PruebaTrunkbase/PruebaTrunkbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.trunkbase.PruebaTrunkbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTrunkbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
