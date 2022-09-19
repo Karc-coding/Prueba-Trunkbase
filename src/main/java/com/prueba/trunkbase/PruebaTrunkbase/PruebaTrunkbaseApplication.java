8cfb47e6226f235255dd2fc339d2a7db639d537e
Karc <kevinrojascobenas@gmail.com>
2022-09-19 12:27:05 -0500

    commit rama1

--- src/main/java/com/prueba/trunkbase/PruebaTrunkbase/PruebaTrunkbaseApplication.java
+++ src/main/java/com/prueba/trunkbase/PruebaTrunkbase/PruebaTrunkbaseApplication.java
@@ -8,7 +8,7 @@ public class PruebaTrunkbaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaTrunkbaseApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello World!!!");
 		
 	}
 
